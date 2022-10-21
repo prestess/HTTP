dfecf7e1f32f88688bbe837dc3cb02a498354d50
prestess <50713068+prestess@users.noreply.github.com>
2022-10-21 19:11:56 -0300

    Add files via upload

---
+++ tampermonkey-cabeca-alt.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         alt
+// @namespace    felipe-netbook
+// @version      0.2
+// @description  q bom tudo tranquilo
+// @author       felipe
+// @icon
+// @grant        none
+// @require   https://raw.githubusercontent.com/prestess/HTTP/main/tampermonkey-corpo-alt.js
+// @match        https://www.invertexto.com/netbook
+// @match        http://netbooks.networkmedicine.org/*/Welcome_to_netBooks.ipynb*
+// @match        http://netbooks.networkmedicine.org/*/Untitled*
+// @match        http://netbooks.networkmedicine.org/user/*/
+// @match        http://netbooks.networkmedicine.org/hub/spawn-pending/*
+// ==/UserScript==
+//**************
+// @require   https://raw.githubusercontent.com/prestess/HTTP/main/tampermonkey-corpo-alt.js
+//****************
