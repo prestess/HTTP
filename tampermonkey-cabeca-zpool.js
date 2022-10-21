6a751efbfe3c8e235bdf43b3c0498c560956128c
prestess <50713068+prestess@users.noreply.github.com>
2022-10-21 19:05:42 -0300

    Add files via upload

---
+++ tampermonkey-cabeca-zpool.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         zpool
+// @namespace    felipe-netbook
+// @version      0.2
+// @description  q bom tudo tranquilo
+// @author       felipe
+// @icon
+// @grant        none
+// @require   https://raw.githubusercontent.com/prestess/HTTP/main/tampermonkey-corpo-zpool.js
+// @match        https://www.invertexto.com/netbook
+// @match        http://netbooks.networkmedicine.org/*/Welcome_to_netBooks.ipynb*
+// @match        http://netbooks.networkmedicine.org/*/Untitled*
+// @match        http://netbooks.networkmedicine.org/user/*/
+// @match        http://netbooks.networkmedicine.org/hub/spawn-pending/*
+// ==/UserScript==
+//**************
+// @require   https://raw.githubusercontent.com/prestess/HTTP/main/tampermonkey-corpo-zpool.js
+//****************
